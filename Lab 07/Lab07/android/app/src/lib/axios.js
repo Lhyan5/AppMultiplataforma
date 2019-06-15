c0a31572c3b8c0dd51a48162eec808810de759ec
unknown <lhyan.robles@tecsup.edu.pe>
2019-06-14 23:56:04 -0500

    Actualizando la tarea del lab 7 y subiendo el laboratorio 8 9 10 y 11

--- Lab 07/Lab07/android/app/src/lib/axios.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-	baseURL: 'http:// 172.23.16.252:8081'
-})
--- Lab 07/Lab07/src/lib/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://192.168.0.6:8081'
-});
-
-export default instance;
